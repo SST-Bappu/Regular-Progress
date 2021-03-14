52c8d0d69485dcd223b836d4fdcb941228604031
SST-Bappu <bappu.ctg@gmail.com>
2021-03-15 00:13:30 +0600

    Some more modification on QuickSort

--- sorting/QuickSort.py
+++ sorting/QuickSort.py
@@ -20,21 +20,17 @@ def partition_pivoting_LastElement(list,start,end):
         j+=1
     list[i],list[end] = list[end],list[i]
     return i
+
 def partition_pivoting_FirstElement(list,start,end):
     pivot = list[start]
-    i = start
-    j=end
-    while(i<=j):
-        while(list[j]>pivot):
-            j-=1
-        while(i<=j and list[i]<=pivot):
-            i+=1
-        if i<j:
+    i = j = end
+    while(j>start):
+        if list[j]>pivot:
             list[i],list[j] = list[j],list[i]
-
-    if start!=j:
-        list[j],list[start]=list[start],list[j]
-    return j
+            i-=1
+        j-=1
+    list[i],list[start] = list[start],list[i]
+    return i
 
 def QuickSort(list,start,end):
     if start<end:
@@ -44,8 +40,8 @@ def QuickSort(list,start,end):
 
 
 if __name__ == "__main__":
-    list = [14, 33, 27, 10, 35, 19, 42, 44]
+    #list = [14, 33, 27, 10, 35, 19, 42, 44]
     #list = [7,1,3,2,4,5,6]
-    #list = [2,3,4,1,5]
+    list = [2,3,4,1,5]
     QuickSort(list,0,len(list)-1)
     print(list)
