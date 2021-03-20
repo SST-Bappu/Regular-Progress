44749234cfef191983f94aa843d7f0f461cc63a0
SST-Bappu <bappu.ctg@gmail.com>
2021-03-21 00:01:56 +0600

    Just implemented right side view of a binary tree

---
+++ Master coding interview/problems/BST_to_array_BFS.py
@@ -0,0 +1,29 @@
+import BinaryTree
+def BST_BFS(root):
+    if not root:
+        return []
+    result = []
+    q = [root]
+    while len(q):
+        l = len(q)
+        count = 0
+        Data_every_level = []
+        while count<l:
+            current = q.pop(0)
+            Data_every_level.append(current.data)
+            if current.left:
+                q.append(current.left)
+            if current.right:
+                q.append(current.right)
+            count+=1
+        result.append(Data_every_level)
+    return result
+if __name__=="__main__":
+    list = [5,8,7,6,3,4,2,10,2]
+    Head = BinaryTree.BinaryTree()
+    i=0
+    while(i<len(list)):
+        BinaryTree.InsertNode(Head,list[i])
+        i+=1
+    result = BST_BFS(Head)
+    print(result)
--- Master coding interview/problems/RightSideViewOfBinaryTree.py
+++ Master coding interview/problems/RightSideViewOfBinaryTree.py
@@ -1,29 +1,35 @@
 import BinaryTree
-def BST_DFS(root):
+import BST_to_array_BFS
+def RightSideView(root):
     if not root:
         return []
-    result = []
     q = [root]
+    result = []
     while len(q):
+        temp = []
         l = len(q)
         count = 0
-        Data_every_level = []
         while count<l:
             current = q.pop(0)
-            Data_every_level.append(current.data)
+            temp.append(current.data)
             if current.left:
                 q.append(current.left)
             if current.right:
                 q.append(current.right)
             count+=1
-        result.append(Data_every_level)
+        result.append(temp.pop())
     return result
+
 if __name__=="__main__":
-    list = [5,8,7,6,3,4,2,10,2]
-    Head = BinaryTree.BinaryTree()
-    i=0
-    while(i<len(list)):
-        BinaryTree.InsertNode(Head,list[i])
-        i+=1
-    result = BST_DFS(Head)
+    list = [7,5,2,6,4,3,8,9]
+    tree= BinaryTree.BinaryTree()
+    for item in list:
+        BinaryTree.InsertNode(tree,item)
+    result = BST_to_array_BFS.BST_BFS(tree)
     print(result)
+    result = RightSideView(tree)
+    print(result)
+
+
+
+
