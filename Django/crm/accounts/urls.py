da76a0aca7eecebec9164ef11f7ef2adf0928f59
SST-Bappu <bappu.ctg@gmail.com>
2021-04-01 10:02:06 +0600

    worked on project mostly

--- Django/crm/accounts/urls.py
+++ Django/crm/accounts/urls.py
@@ -4,5 +4,8 @@ urlpatterns = [
     path('',views.home,name="home"),
     path('products/',views.products,name="products"),
     path('customer/<str:pk_test>/',views.customer,name="customer"),
-    path('create')
-]
+    path('createOrder/',views.createOrder,name="createOrder"),
+    path('updateOrder/<str:pk>/',views.updateOrder,name="updateOrder"),
+    path('deleteOrder/<str:pk>/',views.deleteOrder,name="deleteOrder"),
+    ]
+ 
--- Django/crm/accounts/views.py
+++ Django/crm/accounts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from . models import *
+from .form import OrderForm
 # Create your views here.
 
 def home(request):
@@ -22,5 +23,27 @@ def customer(request,pk_test):
     context={'customer':customer,'total_orders':total_orders,'orders':orders}
     return render(request,'accounts/customer.html',context)
 def createOrder(request):
-    context = {}
+    form = OrderForm()
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    context = {'form':form}
     return render(request,'accounts/order_form.html',context)
+def updateOrder(request,pk):
+    order = Order.objects.get(id=pk)
+    form = OrderForm(instance=order)
+    if request.method =='POST':
+        form = OrderForm(request.POST,instance=order)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    context={'form':form}
+    return render(request,'accounts/order_form.html',context)
+def deleteOrder(request,pk):
+    order = Order.objects.get(id=pk)
+    if request.method=="POST":
+        order.delete()
+    return redirect('/')
+
