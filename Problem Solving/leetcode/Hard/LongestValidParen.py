7c5af96ed7eb7684e975fa31187427f2f8f01957
SST-Bappu <bappu.ctg@gmail.com>
2021-05-22 22:51:08 +0600

    Days are going on

--- Problem Solving/leetcode/Hard/LongestValidParen.py
+++ Problem Solving/leetcode/Hard/LongestValidParen.py
@@ -4,7 +4,6 @@ def LongestParen(s):
     maxlen = 0
     for i in range(len(s)):
         if s[i]==')' and i-1>=0 and s[i-1]=='(':
-            print("Got it there")
             dp[i] = dp[i-2]+2 if i>2 else 2
         elif s[i]==')' and s[i-1]==')':
             chk  = i-dp[i-1]-1
@@ -16,7 +15,41 @@ def LongestParen(s):
                     dp[i] = dp[i-1] +2
         maxlen = max(maxlen,dp[i])
     return maxlen
+def LongestParen_sol2(s):
+    maxlen = 0
+    stack = [-1]
+    for i in range(len(s)):
+        if s[i] == ')' and len(stack)>1:
+            stack.pop()
+            maxlen=max(maxlen,(i-stack[len(stack)-1]))
+        elif s[i]=='(':
+            stack.append(i)
+    return maxlen
+def LongestParen_sol3(s):
+    maxlen = 0
+    left = right = 0
+    for i in range(len(s)):
+        if s[i] == '(':
+            left+=1
+        else:
+            right+=1
+        if left==right:
+            maxlen = max(maxlen,left+right)
+        if right>left:
+            right = left = 0
+    left = right = 0
+    for i in range(len(s)-1,-1,-1):
+        if s[i] == '(':
+            left += 1
+        else:
+            right += 1
+        if left == right:
+            maxlen = max(maxlen,left+right)
+        if left>right:
+            right = left = 0
+    return maxlen
+
 if __name__=="__main__":
-    s =")("
-    print(LongestParen(s))
+    s =")()())"
+    print(LongestParen_sol2(s))
 
